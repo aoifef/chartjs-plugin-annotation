8a9777faf873b5e0c1e904d90b74f1f8212fd7b3
Aoife Finnegan <A.Finnegan@kainos.com>
2018-06-28 16:06:17 +0100

    adding more padding

--- chartjs-plugin-annotation.js
+++ chartjs-plugin-annotation.js
@@ -819,8 +819,8 @@ module.exports = function(Chart) {
 			model.labelTitle = options.label.title;
 
 			ctx.font = chartHelpers.fontString(model.labelFontSize, model.labelFontStyle, model.labelFontFamily);
-			var textHeight = model.labelFontSize + 1;
-            if(model.labelContent !== null){
+			var textHeight = model.labelFontSize;
+            if(model.labelContent !== null) {
                 var labelContentArray = model.labelContent.split("\n");
                 if(model.labelTitle !== null){
                     labelContentArray.push(model.labelTitle);
@@ -829,12 +829,16 @@ module.exports = function(Chart) {
                 var longestLabel = labelContentArray.sort(function (a, b) { return b.length - a.length; })[0];
 			    var textWidth = ctx.measureText(longestLabel).width;
 
-                model.labelHeight = (textHeight * labelContentArray.length) + (2 * model.labelYPadding) + (3 * (labelContentArray.length - 1));
-                if(model.labelTitle !== null){
-                    model.labelHeight += 6;
+                model.labelHeight = (textHeight * labelContentArray.length) + (2 * model.labelYPadding);
+                if(labelContentArray.length > 1) {
+                    model.labelHeight += (5 * (labelContentArray.length - 1));
                 }
+                if(model.labelTitle !== null) {
+                    model.labelHeight += 4;
+                }
+
 			} else {
-                var textWidth = ctx.measureText(model.labelContent).width;
+                var textWidth = ctx.measureText(model.labeltTitle).width;
                 model.labelHeight = textHeight + (2 * model.labelYPadding);
 			}
 
@@ -968,7 +972,7 @@ module.exports = function(Chart) {
                         textYPosition
                     );
 
-                    textYPosition += (view.labelFontSize + 3);
+                    textYPosition += (view.labelFontSize + 5);
                 }
 
             }
